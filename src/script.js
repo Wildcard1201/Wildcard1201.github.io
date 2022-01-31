c526b180f28b304938700e866c5ac51a74c05c15
Jason Fuhrer <jf.33@hotmail.com>
2022-01-31 15:29:05 -0500

    fixed rotation

--- src/script.js
+++ src/script.js
@@ -1,6 +1,7 @@
 const btnRotate = document.getElementById('btn-rotate')
 const rotate = () => {
     btnRotate.style.transform = 'rotate(90deg)';
+    console.log('function fired')
 }
 
-btnRotate.addEventListener('click', rotate())
+btnRotate.addEventListener('click', rotate)
