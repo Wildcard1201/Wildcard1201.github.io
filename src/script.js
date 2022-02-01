e0849dbdd20f82867f7b4da47c8fa494595440f6
Jason Fuhrer <jf.33@hotmail.com>
2022-02-01 08:57:23 -0500

    who knows?

--- src/script.js
+++ src/script.js
@@ -1,10 +1,16 @@
 const btnRotate = document.getElementById('btn-rotate')
+const goofy = document.getElementById('goofy')
 
 let rotation = 0;
+let font = 12;
 const rotate = () => {
     rotation += 90;
     btnRotate.style.transform = `rotate(${rotation}deg)`;
     console.log('function fired')
+    font += 2;
+    goofy.style.fontSize = `${font}px`
 }
 
 btnRotate.addEventListener('click', rotate)
+
+
